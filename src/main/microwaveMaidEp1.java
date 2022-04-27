dfc2f05bd7293c60b9a149516b4c74ec046f2569
Nosieek <mateusz.nosko13@gmail.com>
2022-04-27 21:40:39 +0200

    Added solution

---
+++ src/main/microwaveMaidEp1.java
@@ -0,0 +1,48 @@
+package main;
+
+public class microwaveMaidEp1 {
+    public String getBestCombination(int time) {
+        int minute = time / 60;
+        int seconds = time % 60;
+
+        if (counter(firstMethod(minute,seconds)) == counter(secondMethod(minute, seconds)))
+        {
+            return shortest(firstMethod(minute,seconds), secondMethod(minute, seconds));
+
+        }else if(counter(firstMethod(minute,seconds)) < counter(secondMethod(minute, seconds))){
+
+            return firstMethod(minute,seconds);
+
+        }else if(secondMethod(minute, seconds).length() > 4) {
+
+            return firstMethod(minute,seconds);
+
+        }
+        return secondMethod(minute, seconds);
+    }
+
+    private String firstMethod(int minute, int seconds) {
+        return String.format("%d%02d", minute, seconds).replaceAll("^0+", "");
+    }
+
+    private String secondMethod(int minutes, int seconds) {
+        return String.format("%d%02d", minutes - 1, seconds + 60).replaceAll("^0+", "");
+    }
+    private String shortest(String ourNum, String otherNum){
+        return otherNum.length() < ourNum.length() ? otherNum : ourNum;
+    }
+
+
+    private int counter(String ourNumber) {
+        int counter = 1;
+        for (int i = 1; i < ourNumber.length(); i++) {
+            if (!(ourNumber.charAt(i) == ourNumber.charAt(i - 1))) {
+                counter++;
+            }
+        }
+        // System.out.println(counter);
+        return counter;
+    }
+
+}
+
---
+++ src/test/microwaveMaidEp1Test.java
@@ -0,0 +1,29 @@
+package test;
+import main.microwaveMaidEp1;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class microwaveMaidEp1Test {
+    public microwaveMaidEp1 t = new microwaveMaidEp1();
+
+    @Test
+    public void BasicKata() {
+        assertEquals("6", t.getBestCombination(6));
+        assertEquals("33", t.getBestCombination(33));
+        assertEquals("60", t.getBestCombination(60));
+        assertEquals("111", t.getBestCombination(71));
+        assertEquals("72", t.getBestCombination(72));
+        assertEquals("90", t.getBestCombination(90));
+        assertEquals("99", t.getBestCombination(99));
+        assertEquals("155", t.getBestCombination(115));
+        assertEquals("201", t.getBestCombination(121));
+        assertEquals("300", t.getBestCombination(180));
+        assertEquals("433", t.getBestCombination(273));
+        assertEquals("399", t.getBestCombination(279));
+        assertEquals("6130", t.getBestCombination(3690));
+        assertEquals("7777", t.getBestCombination(4697));
+        assertEquals("8735", t.getBestCombination(5255));
+    }
+
+}
